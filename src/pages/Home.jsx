06d08f66e82a27537852f12044b32213099be7ef
Shubham <cshubham1911@gmail.com>
2022-03-20 21:03:17 +0530

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Popular from '../components/Popular'
+import Veggie from '../components/Veggie'
+
+function Home() {
+  return (
+    <div>
+    <Veggie />
+    <Popular />
+    
+    </div>
+  )
+}
+
+export default Home
