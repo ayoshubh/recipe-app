55c0b3458cd0af4d6536f002723d465a3f562aa1
Shubham Chauhan <cshubham1911@gmail.com>
2022-03-21 18:53:24 +0530

    routing added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Category from './components/Category';
 import Pages from './pages/Pages';
-
+import {  BrowserRouter as Router } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <Category />
       <Pages />
+      </Router>
     </div>
   );
 }
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Home from './Home'
+import {  Routes, Route } from "react-router-dom"
+import Cuisine from './Cuisine'
 
 function Pages() {
   return (
     <div>
-        <Home />
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="cuisine/:type" element={<Cuisine />} >
+          
+        </Route>
+        </Routes>
+      
     </div>
   )
 }
